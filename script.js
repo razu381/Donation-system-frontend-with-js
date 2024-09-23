a0a8fe29c5a2e47ff30545dc87d4bf6c76cf2ec1
shohidul islam <sirazu52@gmail.com>
2024-09-24 03:23:52 +0600

    initial with 1 card

---
+++ script.js
@@ -0,0 +1,56 @@
+let total_balance = 5500
+let  amount_noakhali = 0.00;
+
+//Toggle button for donation and history section
+document.querySelector(".history-container").classList.add('hidden')
+//donation listener
+document.querySelector("#donation-display").addEventListener('click',function(e) {
+    document.querySelector(".cards-container").classList.remove('hidden')
+    document.querySelector(".history-container").classList.add('hidden')
+
+    //changing button styles
+    document.querySelector("#donation-display").classList = ""
+    document.querySelector("#donation-display").classList.add("btn", "bg-green", "mr-5", "text-xl", "font-semibold")
+
+    document.querySelector("#history-display").classList = ""
+    document.querySelector("#history-display").classList.add("btn","bg-transparent", "border", "border-[#111111]", "border-opacity-30", "font-medium")
+
+})
+
+
+//history listener
+document.querySelector("#history-display").addEventListener('click',function(e) {
+    document.querySelector(".history-container").classList.remove('hidden')
+    document.querySelector(".cards-container").classList.add('hidden')
+
+    //adding buttons styles
+    
+    document.querySelector("#history-display").classList = ""
+    document.querySelector("#history-display").classList.add("btn", "bg-green", "ml-5", "text-xl", "font-semibold")
+
+    document.querySelector("#donation-display").classList = ""
+    document.querySelector("#donation-display").classList.add( "btn", "bg-transparent", "border", "border-[#111111]", "border-opacity-30", "font-medium")
+})
+
+
+//Event listerner for Noakhali donation
+document.querySelector("#noakhali-submit").addEventListener('click',function(e) {
+    e.preventDefault()
+    addDonation(".card-noakhali")
+    displayHistory(".card-noakhali")
+    document.querySelector(`.card-noakhali .input`).value = ""
+})
+//Event listerner for feni donation
+document.querySelector("#feni-submit").addEventListener('click',function(e) {
+    e.preventDefault()
+    addDonation(".card-feni")
+    displayHistory(".card-feni")
+    document.querySelector(`.card-feni .input`).value = ""
+})
+//Event listerner for quota movement
+document.querySelector("#quota-submit").addEventListener('click',function(e) {
+    e.preventDefault()
+    addDonation(".card-quota")
+    displayHistory(".card-quota")
+    document.querySelector(`.card-quota .input`).value = ""
+})
---
+++ shared.js
@@ -0,0 +1,41 @@
+function addDonation(cardId){
+    let amount = parseFloat(document.querySelector(`${cardId} .input`).value) 
+    if(amount > 0 && !isNaN(amount) && amount <= total_balance){
+        amount_noakhali += amount;
+        document.querySelector(`${cardId} .local-money-field #amount`).innerText = amount_noakhali
+
+        total_balance -= amount;
+        document.querySelector(`header .total-money-field #total-amount`).innerText = total_balance
+        //show the success modal
+        document.querySelector("#donation_success_modal").showModal()
+
+    }else{
+        document.querySelector("#donation_fail_modal").showModal()
+    }
+    
+}
+
+
+function displayHistory(cardId){
+    console.log("Displaying history")
+    let amount = parseFloat(document.querySelector(`${cardId} .input`).value)
+    console.log(`amount is ${amount}`) 
+    let title = document.querySelector(`${cardId} .donation-title`).innerText
+
+    const currentDate = new Date();
+    const bdFormat = currentDate.toLocaleString('en-US', {
+    timeZone: 'Asia/Dhaka',
+    timeZoneName: 'short',
+    hour12: true
+    });
+
+    let div = document.createElement('div')
+    div.classList.add("border", "border-black", "border-opacity-10","p-8", "rounded-2xl", "mb-6")
+    div.innerHTML = `<h1>${amount} Taka is Donated for ${title}</h1>
+                <p class="font-light text-base">
+                     Date : ${bdFormat}
+                </p>`
+    
+
+    document.querySelector(".history-container").appendChild(div)
+}
